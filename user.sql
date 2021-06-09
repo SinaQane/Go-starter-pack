4711a48f2cadc7e4d1b436fe5b9b9d371a838909
Sina Ghaseminejad <sina.qane@yahoo.com>
2021-06-09 19:41:50 +0430

    Create user.sql
    SQL file for my_sql.go code

---
+++ user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `name` text,
+    `age` int,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
